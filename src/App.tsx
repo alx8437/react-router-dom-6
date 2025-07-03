88c080fd02b22539ebdc69d1ee4006846d55e7b0
Aleks Zelenskiy <dezion777@gmail.com>
2025-07-03 13:26:36 +0300

    Basic Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne.tsx";
+import {PageTwo} from "./components/pages/PageTwo.tsx";
+import {PageThree} from "./components/pages/PageThree.tsx";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Error404} from "./components/pages/Error404.tsx";
 
 export const App = ()=> {
   return (
@@ -9,7 +14,16 @@ export const App = ()=> {
           Здесь будет навигация
         </div>
         <div className={styles.content}>
-          Здесь будут кроссовки
+            <Routes>
+                <Route path="/" element={<Navigate to={'/page1'} />} />
+                <Route path={'page1'} element={<PageOne />}/>
+                <Route path={'page2'} element={<PageTwo />}/>
+                <Route path={'page3'} element={<PageThree />}/>
+                <Route path={'error404'} element={<Error404 />}/>
+
+                <Route path={'*'} element={<Navigate to={'/error404'} />}/>
+            </Routes>
+
         </div>
       </div>
       <div className={styles.footer}>abibas 2023</div>
