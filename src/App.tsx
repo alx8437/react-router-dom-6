e6e16d9030d01bcea8fb19fdeabbedc0ae336c9b
Aleks Zelenskiy <dezion777@gmail.com>
2025-09-30 16:13:52 +0300

    isActive classname for NavLink

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,13 @@ export const App = ()=> {
       <div className={styles.body}>
         <div className={styles.nav}>
             <div>
-                <NavLink to='page1'>Page1</NavLink>
+                <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='page1'>Page1</NavLink>
             </div>
             <div>
-                <NavLink to='page2'>Page2</NavLink>
+                <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='page2'>Page2</NavLink>
             </div>
             <div>
-                <NavLink to='page3'>Page3</NavLink>
+                <NavLink className={({isActive}) => isActive ? styles.activeNavLink : styles.navLink} to='page3'>Page3</NavLink>
             </div>
         </div>
         <div className={styles.content}>
